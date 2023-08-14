20311ee0bc6fae470864f6966d7563fb2ee47543
AdamKaczmarski <adam.kacz10@gmail.com>
2023-08-14 22:32:17 +0100

    playing while downloading still not it

--- audio-stream-grpc-rs/src/client.rs
+++ audio-stream-grpc-rs/src/client.rs
@@ -2,6 +2,8 @@ pub mod audiostream {
     tonic::include_proto!("audiostream");
 }
 
+use std::io::BufReader;
+
 use audiostream::{audio_streamer_client::AudioStreamerClient, EmptyRequest, TrackRequest};
 use rodio::{Decoder, OutputStream, Sink};
 use tonic::transport::Channel;
@@ -21,33 +23,52 @@ struct TrackStreamed {
 // }
 async fn get_track_streamed(
     client: &mut AudioStreamerClient<Channel>,
-    track: &String
-) -> Result<TrackStreamed, Box<dyn std::error::Error>> {
-    let mut stream = client.stream_track(TrackRequest{track_name: track.to_owned()}).await?.into_inner();
-    let mut data: Vec<f32> = Vec::new();
-    let mut frequency: u32 = 0;
-    let mut channels: u32 = 0;
+    track: &String,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let (_stream, stream_handler) =
+        OutputStream::try_default().expect("Couln't obtain default playback device");
+    let sink: Sink = Sink::try_new(&stream_handler).unwrap();
+
+    let mut stream = client
+        .stream_track(TrackRequest {
+            track_name: track.to_owned(),
+        })
+        .await?
+        .into_inner();
+    // let mut frequency: u32 = 0;
+    // let mut channels: u32 = 0;
     println!("STARTED STREAMING");
     while let Some(msg) = stream.message().await? {
-        msg.track_byte.iter().for_each(|byte|{
-            data.push(byte.clone())
-        });
-        frequency = msg.frequency;
-        channels = msg.channels;
+        let mut data: Vec<f32> = Vec::new();
+        println!("msg");
+        msg.track_byte
+            .iter()
+            .for_each(|byte| data.push(byte.clone()));
+        // frequency = msg.frequency;
+        // channels = msg.channels;
+        let source = rodio::buffer::SamplesBuffer::new(msg.channels as u16, msg.frequency, data);
+        sink.append(source);
+        sink.set_volume(0.03);
+        if sink.is_paused() {
+            sink.play();
+        }
     }
+    while !sink.empty(){
+        // println!("is paused? {}",sink.is_paused());
+        // sink.play();
+    }
+    sink.stop();
+
     println!("DONE STREAMING");
-    println!("Length of streamed data={}", data.len());
+    // println!("Length of streamed data={}", data.len());
 
-    return Ok(TrackStreamed {
-        frequency,
-        channels,
-        bytes: data,
-    });
+    return Ok(());
 }
-async fn get_track_list(client:& mut AudioStreamerClient<Channel>) -> Result<Vec<String>,Box<dyn std::error::Error>>{
-    let response = client.get_track_list(EmptyRequest{}).await?;
+async fn get_track_list(
+    client: &mut AudioStreamerClient<Channel>,
+) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let response = client.get_track_list(EmptyRequest {}).await?;
     return Ok(response.into_inner().track_names);
-
 }
 
 #[tokio::main]
@@ -57,20 +78,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("{:?}", track_list);
 
     // let downloaded_track: Track = get_track(&mut client).await?;
-    let downloaded_track: TrackStreamed = get_track_streamed(&mut client,track_list.get(0).unwrap()).await?;
-
-    let (_stream, stream_handler) =
-        OutputStream::try_default().expect("Couln't obtain default playback device");
-    let sink: Sink = Sink::try_new(&stream_handler).unwrap();
-    let source = rodio::buffer::SamplesBuffer::new(
-        downloaded_track.channels as u16,
-        downloaded_track.frequency,
-        downloaded_track.bytes,
-    );
-    sink.append(source);
-    sink.set_volume(0.3);
-
-    sink.sleep_until_end();
+    _ = get_track_streamed(&mut client, track_list.get(1).unwrap()).await?;
 
     return Ok(());
 }
